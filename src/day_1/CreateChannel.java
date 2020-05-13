fd26cfd8a3a6da5e6d643691382ce4f47df6c379
fatihyildirim71 <fatihyildirim71@gmail.com>
2020-05-14 01:39:42 +0200

    add CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,4 +4,8 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("\"Hello slackk\" = " + "Hello slackk");
     }
+
+
+    // if )write  code it ok from intellij
+    // if )write  code it ok from intellij
 }
